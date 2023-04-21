4bc804db3ba694cc761f59624ee30887279c80fb
Vimaleshwar04 <131465515+Vimaleshwar04@users.noreply.github.com>
2023-04-21 19:43:39 +0530

    background added

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,9 +1,11 @@
-import React from 'react'
-import Input from './Input'
+import React from "react";
+import Input from "./Input";
 
-function Dashboard({
-  // mobileList=[]
-}) {
+function Dashboard(
+  {
+    // mobileList=[]
+  }
+) {
   // const [mobileList, setmobileList] = useState([])
   // useEffect(() => {
   //   axios.get("GetApi")
@@ -14,26 +16,22 @@ function Dashboard({
   // }, []);
   let mobileList = JSON.parse(sessionStorage.getItem("mobileList"));
   return (
-    <div className="Container">
-      <div className='home_page'>
-      <h1 className="Header">Mobile Number List</h1>
-      {
-        mobileList.map((e,i)=>{
-          return (
-            <Input
-            key={i}
-              value={e}
-              name=""
-              disabled={true}
-            />
-          );
-        })
-      }
-        
+    <div
+      style={{
+        backgroundImage: "linear-gradient(#edf6fb, white)",
+        height: "100vh",
+      }}
+    >
+      <div className="Container">
+        <div className="home_page">
+          <h1 className="Header">Mobile Number List</h1>
+          {mobileList.map((e, i) => {
+            return <Input key={i} value={e} name="" disabled={true} />;
+          })}
+        </div>
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
